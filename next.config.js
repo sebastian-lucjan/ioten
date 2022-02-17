681297146a5f88d880b6c06af65cf12af4e0c675
sebastian-lucjan <sebastian.lucjan@gmail.com>
2022-02-17 15:14:31 +0100

    Config eslint, prettier, Fix all files by eslint

--- next.config.js
+++ next.config.js
@@ -1,42 +1,41 @@
 const { PHASE_PRODUCTION_BUILD } = require('next/constants');
 
-// module.exports = (phase) => {
-module.exports = () => {
-    const reactStrictMod = true;
+module.exports = (phase) => {
+  const reactStrictMod = true;
 
-    // const isDev = phase === PHASE_DEVELOPMENT_SERVER;
-    const isProd = phase === PHASE_PRODUCTION_BUILD;
+  // const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+  const isProd = phase === PHASE_PRODUCTION_BUILD;
 
-    const env = {
+  const env = {
     //     SITE_KEY: process.env.SITE_KEY,
     //     SECRET_KEY: process.env.SECRET_KEY,
     //     MAIL_USER: process.env.MAIL_USER,
     //     MAIL_PASSWORD: process.env.MAIL_PASSWORD,
-        IS_PROD: isProd,
+    IS_PROD: isProd,
     //     NEXT_PUBLIC_GA_MEASUREMENT_ID: process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID,
     //     NEXT_PUBLIC_GA_VERCEL_ID: process.env.NEXT_PUBLIC_GA_VERCEL_ID,
-    };
-    //
-    const rewrites = () => {
-        return [
-    //         {
-    //             source: '/o-nas',
-    //             destination: '/about',
-    //         },
-    //         {
-    //             source: '/oferta',
-    //             destination: '/offer',
-    //         },
-    //         {
-    //             source: '/kontakt',
-    //             destination: '/contact',
-    //         },
-        ];
-    };
+  };
+  //
+  const rewrites = () => {
+    return [
+      //         {
+      //             source: '/o-nas',
+      //             destination: '/about',
+      //         },
+      //         {
+      //             source: '/oferta',
+      //             destination: '/offer',
+      //         },
+      //         {
+      //             source: '/kontakt',
+      //             destination: '/contact',
+      //         },
+    ];
+  };
 
-    return {
-        reactStrictMod,
-        env,
-        rewrites,
-    };
+  return {
+    reactStrictMod,
+    env,
+    rewrites,
+  };
 };
