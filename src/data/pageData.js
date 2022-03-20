eef9d4c65b5bbf7fc4905f0bb574460759b4eb5f
sebastian-lucjan <sebastian.lucjan@gmail.com>
2022-03-20 13:16:43 +0100

    Add pageData.js

---
+++ src/data/pageData.js
@@ -0,0 +1,32 @@
+export const navigationData = {
+  pages: ['Usługi', 'O nas', 'Blog', 'Kontakt'],
+};
+
+export const services = [
+  { heading: 'Strona wizytówka' },
+  { heading: 'Strona firmowa z CMSem' },
+  { heading: 'Sklep online' },
+  { heading: 'Landingpage' },
+  { heading: 'Blog' },
+];
+
+export const serviceStages = [
+  { heading: 'Idea & Strategia' },
+  { heading: 'Koncept & Wireframing' },
+  { heading: 'Design' },
+  { heading: 'Development' },
+  { heading: 'Optymalizacje & Wsparcie' },
+];
+
+export const footerData = {
+  intro: {
+    heading: 'Napisz do nas...',
+    text: 'Chcesz otworzyć swój biznes online? Może chcesz się nas o coś zapytać? A może po prostu podoba Ci się nasza strona, śmiało! Napisz do nas na pewno Ci odpowiemy.',
+  },
+  contact: {
+    cooperation: { heading: 'Współpraca / oferta', mail: 'info@ioten.io', mobile: '863 456 331' }, // mobile placeholder
+    other: { heading: 'Inne', mail: 'hello@ioten.io', mobile: '602 446 335' },
+    job: { heading: 'Praca', mail: 'career@ioten.io' },
+  },
+  socialMedia: {},
+};
