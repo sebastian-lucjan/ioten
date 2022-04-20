4fec6f5f09f5d0b81914298149933958527959fc
sebastian-lucjan <sebastian.lucjan@gmail.com>
2022-04-20 13:41:25 +0200

    Done Policy and term of use rosponsivness

--- pages/policy/index.jsx
+++ pages/policy/index.jsx
@@ -3,33 +3,35 @@ import policyData from 'src/data/policyData';
 import { NextSeo } from 'next-seo';
 import BaseLayout from 'src/components/BaseLayout';
 import Grid from 'src/components/Grid';
-import styled from 'styled-components';
 import { TextHeading } from 'src/components/TextComponents';
 import PolicySection from 'src/components/PolicySection';
-import theme from '../../src/assets/styles/theme';
-
-const Wrapper = styled.section`
-  position: relative;
-  padding: 80px 40px 40px 40px;
-`;
+import theme from 'src/assets/styles/theme';
+import IdeaInterlude from 'src/components/IdeaInterlude';
+import MainSectionWrapper from 'src/components/MainSectionWrapper/MainSectionWrapper.styles';
+import { ContentContainer, Wrapper } from 'src/components/Policy/Policy.styles';
 
 export default function PolicyPage() {
   const { heading, sectionsArray, metaData } = policyData;
 
   return (
-    <BaseLayout footerGridColor={theme.color.lightGray}>
+    <MainSectionWrapper background={theme.color.white}>
       <NextSeo title={metaData.title} description={metaData.description} />
-      <Wrapper>
-        <TextHeading size="lg" as="h1">
-          {heading}
-        </TextHeading>
-        <ol>
-          {sectionsArray.map((section) => (
-            <PolicySection sectionData={section} key={uuid()} />
-          ))}
-        </ol>
-        <Grid />
-      </Wrapper>
-    </BaseLayout>
+      <BaseLayout footerGridColor={theme.gradient.yellowToGray}>
+        <Wrapper>
+          <ContentContainer>
+            <TextHeading size="lg" as="h1">
+              {heading}
+            </TextHeading>
+            <ol>
+              {sectionsArray.map((section) => (
+                <PolicySection sectionData={section} key={uuid()} />
+              ))}
+            </ol>
+            <Grid colors={{ lines: theme.gradient.grayToYellow }} />
+          </ContentContainer>
+        </Wrapper>
+        <IdeaInterlude />
+      </BaseLayout>
+    </MainSectionWrapper>
   );
 }
