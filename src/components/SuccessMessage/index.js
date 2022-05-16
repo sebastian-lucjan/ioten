cb936006bae530199b615cb3ddc967adf47c8993
sebastian-lucjan <sebastian.lucjan@gmail.com>
2022-05-16 12:26:08 +0200

    Add show message after sending services contact message

--- src/components/SuccessMessage/index.js
+++ src/components/SuccessMessage/index.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { useTimeout } from '../../hooks/useTimeout';
+import { useTimeout } from 'src/hooks/useTimeout';
 
 const Wrapper = styled.div`
   position: fixed;
@@ -8,28 +8,36 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
   width: calc(100vw - 80px);
-  height: 120px;
+  height: 160px;
   z-index: 100;
   background-color: ${({ theme }) => theme.color.green};
   color: ${({ theme }) => theme.color.white};
   font-family: ${({ theme }) => theme.font.family.myriadPro.bold};
-  font-size: ${({ theme }) => theme.font.size.smallText}; ;
+  font-size: ${({ theme }) => theme.font.size.smallText};
+  border-radius: 6px;
+
+  ${({ theme }) => theme.mq.tablet} {
+    width: calc(50vw);
+    left: 25%;
+    font-size: ${({ theme }) => theme.font.size.paragraph};
+  }
 `;
 
 const StyledTimeBar = styled.div`
   position: relative;
   width: 100%;
   height: 6px;
-  margin-top: 2rem;
+  margin-top: 3rem;
   border: 1px solid ${({ theme }) => theme.color.white};
   overflow: hidden;
+  border-radius: 6px;
 
   span {
     position: absolute;
     background-color: ${({ theme }) => theme.color.white};
     width: 100%;
     height: 6px;
-    animation: move 4.9s linear 0s 1 forwards;
+    animation: move 5s linear 0s 1 forwards;
   }
 
   @keyframes move {
