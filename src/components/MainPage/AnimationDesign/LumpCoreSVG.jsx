85e696a08ca57a2706da82ba13422d1d32cb5a7e
sebastian-lucjan <sebastian.lucjan@gmail.com>
2022-05-20 08:11:22 +0200

    Work on design animation: move circel core on device move

--- src/components/MainPage/AnimationDesign/LumpCoreSVG.jsx
+++ src/components/MainPage/AnimationDesign/LumpCoreSVG.jsx
@@ -3,18 +3,40 @@
 // import useCircleTranslateCords from "src/hooks/useCircleTranslateCords";
 // import useDeviceCoordinates from "src/hooks/useDeviceCoordinates";
 
-import useLumpCoordinates from "src/hooks/useLumpCoordinates";
+import useLumpCoordinates from 'src/hooks/useLumpCoordinates';
 // import { useDeviceOrientation } from "../../../hooks/useDeviceOrientation";
+import { useEffect, useRef } from 'react';
+import gsap from 'gsap';
 
 export default function LumpCoreSVG() {
+  const imageRef = useRef(null);
   const { x, y } = useLumpCoordinates();
   // const { x, y } = useDeviceCoordinates();
+
+  useEffect(() => {
+    const { current: el } = imageRef;
+
+    const circe = el.getElementById('core');
+
+    console.log('circe', circe);
+    console.log('x', x);
+    console.log('y', y);
+
+    // tl.current = gsap.timeline();
+
+    // tl.current.to(circe,{x, y});
+    // tl.current.resume();
+    gsap.to(circe, {
+      x: x - 200,
+      y: y - 200,
+    });
+  }, [x, y]);
 
   // const { orientation, requestAccess, revokeAccess, error } = useDeviceOrientation();
   // console.log(orientation);
 
   return (
-    <svg viewBox="0 0 363 390" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg ref={imageRef} viewBox="0 0 363 390" fill="none" xmlns="http://www.w3.org/2000/svg">
       <g id="lump-core">
         <path
           id="back-edges"
--- src/hooks/useLumpCoordinates.js
+++ src/hooks/useLumpCoordinates.js
@@ -4,72 +4,75 @@ const useLumpCoordinates = () => {
   const [coordinates, setCoordinates] = useState({ x: 160, y: 160 });
 
   useEffect(() => {
-    if (window.innerWidth > 768) {
-      const handleMouseMove = (e) => {
-        const screenUsedWidth = window.innerWidth <= 768 ? window.innerWidth : window.innerWidth / 2;
+    // if (window.matchMedia('(pointer: coarse)').matches) {
+    //   // touchscreen
+    //   console.log('touch screen');
+    // }
+    if (window.matchMedia('(pointer: coarse)').matches) {
+      const handleChangeDevicesOrientation = (e) => {
+        const { beta, gamma } = e;
 
-        const cursorX = e.clientX;
-        let xCord = 140 + (cursorX * 100) / screenUsedWidth;
+        let xCord;
+        let yCord;
 
-        if (xCord > 240) {
-          xCord = 240;
+        if (beta >= -90 && beta <= 90) {
+          yCord = 200 - (5 * beta) / 4;
+          // yCord = 200 - (3 * beta) / 4;
         }
-
-        const cursorY = e.clientY;
-        let yCord = 150 + (cursorY * 100) / screenUsedWidth;
-
-        if (yCord > 250) {
-          yCord = 250;
+        if (beta < -90) {
+          yCord = 200 + (5 * (beta + 180)) / 4;
+          // yCord = 200 + (3 * (beta + 180)) / 4;
+        }
+        if (beta > 90) {
+          yCord = 200 + (5 * (beta - 180)) / 4;
+          // yCord = 200 + (3 * (beta - 180)) / 4;
+        }
+        if (gamma >= 0) {
+          xCord = 190 - gamma;
+        }
+        if (gamma < 0) {
+          xCord = 190 - gamma;
+        }
+        if (gamma >= 0 && (beta >= 90 || beta <= -90)) {
+          xCord = 190 + gamma;
+        }
+        if (gamma < 0 && (beta >= 90 || beta <= -90)) {
+          xCord = 190 + gamma;
         }
 
         setCoordinates({ x: xCord, y: yCord });
       };
 
-      window.addEventListener('mousemove', handleMouseMove);
+      window.addEventListener('deviceorientation', handleChangeDevicesOrientation);
 
       return () => {
-        window.removeEventListener('mousemove', handleMouseMove);
+        window.removeEventListener('deviceorientation', handleChangeDevicesOrientation);
       };
     }
+    const handleMouseMove = (e) => {
+      const screenUsedWidth = window.innerWidth <= 768 ? window.innerWidth : window.innerWidth / 2;
 
-    const handleChangeDevicesOrientation = (e) => {
-      const { beta, gamma } = e;
+      const cursorX = e.clientX;
+      let xCord = 140 + (cursorX * 100) / screenUsedWidth;
 
-      let xCord;
-      let yCord;
+      if (xCord > 240) {
+        xCord = 240;
+      }
 
-      if (beta >= -90 && beta <= 90) {
-        yCord = 200 - (5 * beta) / 4;
-        // yCord = 200 - (3 * beta) / 4;
-      }
-      if (beta < -90) {
-        yCord = 200 + (5 * (beta + 180)) / 4;
-        // yCord = 200 + (3 * (beta + 180)) / 4;
-      }
-      if (beta > 90) {
-        yCord = 200 + (5 * (beta - 180)) / 4;
-        // yCord = 200 + (3 * (beta - 180)) / 4;
-      }
-      if (gamma >= 0) {
-        xCord = 190 - gamma;
-      }
-      if (gamma < 0) {
-        xCord = 190 - gamma;
-      }
-      if (gamma >= 0 && (beta >= 90 || beta <= -90)) {
-        xCord = 190 + gamma;
-      }
-      if (gamma < 0 && (beta >= 90 || beta <= -90)) {
-        xCord = 190 + gamma;
+      const cursorY = e.clientY;
+      let yCord = 150 + (cursorY * 100) / screenUsedWidth;
+
+      if (yCord > 250) {
+        yCord = 250;
       }
 
       setCoordinates({ x: xCord, y: yCord });
     };
 
-    window.addEventListener('deviceorientation', handleChangeDevicesOrientation);
+    window.addEventListener('mousemove', handleMouseMove);
 
     return () => {
-      window.removeEventListener('deviceorientation', handleChangeDevicesOrientation);
+      window.removeEventListener('mousemove', handleMouseMove);
     };
   }, [coordinates]);
 
