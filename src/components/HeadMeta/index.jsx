2b73021c42c8537ce2d4efa4d71608e44c1bd357
sebastian-lucjan <sebastian.lucjan@gmail.com>
2022-07-07 11:51:28 +0200

    Work on fix GA by relocating Script tags to Home component

--- src/components/HeadMeta/index.jsx
+++ src/components/HeadMeta/index.jsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Script from 'next/script';
+// import Script from 'next/script';
 
 const HeadMeta = () => {
   return (
@@ -23,16 +23,16 @@ const HeadMeta = () => {
       <meta property="og:locale" content="pl_PL" />
 
       {/* <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`} /> */}
-      <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`} strategy="afterInteractive" />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
+      {/* <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`} strategy="afterInteractive" /> */}
+      {/* <Script id="google-analytics" strategy="afterInteractive"> */}
+      {/*  {` */}
+      {/*    window.dataLayer = window.dataLayer || []; */}
+      {/*    function gtag(){window.dataLayer.push(arguments);} */}
+      {/*    gtag('js', new Date()); */}
 
-          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
-        `}
-      </Script>
+      {/*    gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}'); */}
+      {/*  `} */}
+      {/* </Script> */}
       {/* <script */}
       {/*  defer */}
       {/*  dangerouslySetInnerHTML={{ */}
